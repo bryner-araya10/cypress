f261c3fd32608f6bb68f67a80d88cc2283c78f65
Bryner <bryner_araya10@hotmail.com>
2023-06-19 19:36:50 -0600

    agregando proyecto de cypress

---
+++ cypress/e2e/home.cy.js
@@ -0,0 +1,13 @@
+describe('template spec', () => {
+  it('first', () => {
+    cy.visit('https://example.cypress.io')
+  })
+
+it('second', () => {
+  cy.visit('http://localhost:3000/')
+  })
+
+it('third', () => {
+  cy.visit('https://example.cypress.io')
+  })
+})
